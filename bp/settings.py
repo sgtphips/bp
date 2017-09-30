268cd4686db93fdcecb3de7530eac7877558bd0f
sgtphips <poposhub@gmail.com>
2017-09-30 15:43:44 -0400

    Changed Allowed Host Setting

--- bp/settings.py
+++ bp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k$@%53)0y)nz+g$h0w@(gt6j)mwo3y^%l4pp@e#li2xy+g)-vn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','poposhub.pythonanywhere.com']
 
 
 # Application definition
