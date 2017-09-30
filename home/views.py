12cf84d89371d5eb42c1b0cf1d3fee3aa5168d50
sgtphips <poposhub@gmail.com>
2017-09-30 18:19:27 -0400

    Connected Blog and Home Views

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Summary
 
 # Create your views here.
 
 def home(request):
-	return render(request, 'home/home.html', {})
+	summary = Summary.objects.all()
+	return render(request, 'home/home.html', {'summary': summary})
